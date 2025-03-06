dba37dbbd16e16d72a9a0b5bc2c5da76d1e60701
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-03-06 17:36:19 +0100

    storage-cookbook

--- main.py
+++ main.py
@@ -1,45 +1,95 @@
+import os
+from datetime import datetime
 import flet as ft
+
+# constants
+FLET_APP_STORAGE_DATA = os.getenv("FLET_APP_STORAGE_DATA")
+COUNTER_FILE_PATH = os.path.join(FLET_APP_STORAGE_DATA, "counter.txt")
+FLET_APP_CONSOLE = os.getenv("FLET_APP_CONSOLE")
+
+
+class Counter(ft.Text):
+    def __init__(self, storage_path=COUNTER_FILE_PATH):
+        super().__init__(theme_style=ft.TextThemeStyle.HEADLINE_LARGE)
+        self.storage_path = storage_path
+        self.count = self.__read_from_storage()
+
+    def increment(self):
+        """Increment the counter, store the new value, and return it."""
+        self.count += 1
+        self.update()
+        self.__write_to_storage()
+
+    def before_update(self):
+        super().before_update()
+        self.value = f"Button tapped {self.count} time{'' if self.count == 1 else 's'}"
+
+    def __log(self, action: str, value: int = None):
+        """Log executed action."""
+        if value is None:
+            value = self.count
+        print(f"{datetime.now().strftime('%Y/%m/%d %H:%M:%S')} - {action} = {value}")
+
+    def __read_from_storage(self):
+        """Read counter value. If an error occurs, use 0."""
+        try:
+            with open(self.storage_path, "r") as f:
+                value = int(f.read().strip())
+        except (FileNotFoundError, ValueError): 
+            # file does not exist or int parsing failed
+            value = 0
+
+        self.__log("READ", value)
+        return value
+
+    def __write_to_storage(self):
+        """Write current counter value to storage."""
+        with open(self.storage_path, "w") as f:
+            f.write(str(self.count))
+        self.__log("WRITE")
 
 
 def main(page: ft.Page):
-    # the title of the app
-    page.title = "Flet Counter App"
-
-    # a light/bright theme
-    page.theme_mode = "light"
-
-    # the page's alignment
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    def increment_counter(e):
-        """Increment the value of the counter_text object by 1, and update the UI to reflect these changes."""
-        counter_text.value = str(int(counter_text.value) + 1)
-        page.update()
+    def show_logs(e: ft.ControlEvent):
+        if FLET_APP_CONSOLE is not None:
+            with open(FLET_APP_CONSOLE, "r") as f:
+                dlg = ft.AlertDialog(
+                    title=ft.Text("App Logs"),
+                    content=ft.Text(f.read()),
+                    scrollable=True,
+                )
+                page.open(dlg)
 
-    # the app's appbar
+    counter = Counter()
     page.appbar = ft.AppBar(
-        title=ft.Text(
-            "Flet Demo Home Page", color=ft.colors.WHITE
-        ),  # a title of white color
-        bgcolor=ft.colors.BLUE,  # a blue background color
-        center_title=True,  # center the title || without this, the title will be on the left
+        title=ft.Text("Storage Playground", weight=ft.FontWeight.BOLD),
+        center_title=True,
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE,
+        adaptive=True,
+        actions=[
+            ft.IconButton(
+                icon=ft.Icons.REMOVE_RED_EYE,
+                tooltip="Show logs",
+                visible=FLET_APP_CONSOLE is not None,
+                on_click=show_logs,
+            ),
+        ],
     )
-
-    # text that contains the counter number to be incremented
-    counter_text = ft.Text("0", size=64)
-
-    # the app's FAB
     page.floating_action_button = ft.FloatingActionButton(
-        content=ft.Icon(ft.icons.ADD, color=ft.colors.WHITE),
-        shape=ft.CircleBorder(),  # gives the button a round/circle shape
-        on_click=increment_counter,  # the callback to be executed when this button is clicked
-        tooltip="Increment",  # the text to be shown when this button is hovered
-        bgcolor=ft.colors.BLUE,  # a blue background color
+        icon=ft.Icons.ADD,
+        text="Increment Counter",
+        foreground_color=ft.Colors.WHITE,
+        bgcolor=ft.Colors.BLUE,
+        on_click=lambda e: counter.increment(),
     )
+    page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_FLOAT
 
-    # adding our widgets/controls to the page/UI
-    page.add(ft.Text("You have pushed the button this many times:"), counter_text)
+    page.add(ft.SafeArea(counter))
 
 
-ft.app(target=main)
+ft.app(main)
