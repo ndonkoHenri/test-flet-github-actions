3fecf7f71b42ccd5644bc23bce9c1cc41730d6e7
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-03-29 19:45:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,14 @@
 
 import flet as ft
 
-import flet_permission_handler as fph
-
+import win32com.client
 
 def main(page: ft.Page):
-    page.scroll = ft.ScrollMode.ADAPTIVE
-    page.appbar = ft.AppBar(title=ft.Text("PermissionHandler Tests"))
-    ph = fph.PermissionHandler()
-    page.overlay.append(ph)
-
-    def check_permission(e):
-        o = ph.check_permission(e.control.data)
-        page.add(ft.Text(f"Checked {e.control.data.name}: {o}"))
-
-    def request_permission(e):
-        o = ph.request_permission(e.control.data)
-        page.add(ft.Text(f"Requested {e.control.data.name}: {o}"))
-
-    def open_app_settings(e):
-        o = ph.open_app_settings()
-        page.add(ft.Text(f"App Settings: {o}"))
+    page.appbar = ft.AppBar(title=ft.Text("Playground"))
+    page.horizontal_alignment = page.vertical_alignment = "center"
 
     page.add(
-        ft.OutlinedButton(
-            "Check Microphone Permission",
-            data=fph.PermissionType.MICROPHONE,
-            on_click=check_permission,
-        ),
-        ft.OutlinedButton(
-            "Request Microphone Permission",
-            data=fph.PermissionType.MICROPHONE,
-            on_click=request_permission,
-        ),
-        ft.OutlinedButton(
-            "Open App Settings",
-            on_click=open_app_settings,
-        ),
+        ft.Text("Import was successfull!"),
     )
 
 
