ca4a6de557f710e709664835282b9945b1cd7c30
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-11-04 00:38:58 +0100

    schedule workflow

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 
 import flet as ft
+import flet_ads
+import flet_audio
+import flet_audio_recorder
+import flet_charts
+import flet_datatable2
+import flet_flashlight
+import flet_geolocator
+import flet_lottie
+import flet_map
+import flet_permission_handler
+import flet_rive
+import flet_video
+import flet_webview
 
-import win32com.client
 
 def main(page: ft.Page):
     page.appbar = ft.AppBar(title=ft.Text("Playground"))
     page.horizontal_alignment = page.vertical_alignment = "center"
 
     page.add(
-        ft.Text("Import was successfull!"),
+        ft.SafeArea(
+            content=ft.Text("Hello, world!", weight=ft.FontWeight.BOLD),
+        )
     )
 
 
-ft.app(main)
+ft.run(main)
 
 
