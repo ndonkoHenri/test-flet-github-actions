42c2fa7eea687de87a3f2a7427e6ec8c57aecead
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2026-02-16 15:08:15 +0100

    gh test

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.appbar = ft.AppBar(title=ft.Text("Playground"))
+    page.appbar = ft.AppBar(title="Playground", center_title=True)
     page.horizontal_alignment = page.vertical_alignment = "center"
 
     page.add(
         ft.SafeArea(
-            content=ft.Text("Hello, world!", weight=ft.FontWeight.BOLD),
+            content=ft.Text("Hello, world!", weight=ft.FontWeight.BOLD, size=25),
         )
     )
 
