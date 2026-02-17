150e53c733f3c0b67d4a8873f5bef1270d86d770
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2026-02-17 15:54:08 +0100

    try selenium

--- src/main.py
+++ src/main.py
@@ -6,9 +6,16 @@ def main(page: ft.Page):
     page.appbar = ft.AppBar(title=ft.Text("Playground"))
     page.horizontal_alignment = page.vertical_alignment = "center"
 
+    def handle_click():
+        from selenium import webdriver
+
+        driver = webdriver.Chrome()
+        driver.get('https://selenium.dev/')
+        driver.quit()
+
     page.add(
         ft.SafeArea(
-            content=ft.Text("Hello, world!", weight=ft.FontWeight.BOLD),
+            content=ft.TextButton("Launch Chrome Browser", on_click=handle_click),
         )
     )
 
