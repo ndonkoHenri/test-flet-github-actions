8982ea35877b2ab643b317b3491688fe160e1994
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2026-02-25 01:45:21 +0100

    test flet-audio

--- src/main.py
+++ src/main.py
@@ -1,21 +1,14 @@
 
 import flet as ft
-
+import flet_audio as fta 
 
 def main(page: ft.Page):
     page.appbar = ft.AppBar(title=ft.Text("Playground"))
     page.horizontal_alignment = page.vertical_alignment = "center"
 
-    def handle_click():
-        from selenium import webdriver
-
-        driver = webdriver.Chrome()
-        driver.get('https://selenium.dev/')
-        driver.quit()
-
     page.add(
         ft.SafeArea(
-            content=ft.TextButton("Launch Chrome Browser", on_click=handle_click),
+            content=ft.TextButton("Hello World!"),
         )
     )
 
